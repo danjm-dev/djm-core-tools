70dda5d1243604fe3911b60c87f7650c4aeee443
Daniel Miller <danjm.dev@gmail.com>
2024-02-20 12:05:39 +1300

    added labeled icon

--- Editor/UIElements/LabeledIcon.cs
+++ Editor/UIElements/LabeledIcon.cs
@@ -1,9 +1,40 @@
+using UnityEngine;
 using UnityEngine.UIElements;
 
 namespace DJM.CoreTools.Editor.UIElements
 {
     public sealed class LabeledIcon : VisualElement
     {
+        public const string USSClassName = "djm-labeled-icon";
         
+        private TextElement _label;
+        private Image _icon;
+
+        public LabeledIcon() : this(string.Empty)
+        {
+        }
+        
+        public LabeledIcon(string text)
+        {
+            AddToClassList(USSClassName);
+            _label.text = text;
+            
+            _icon = new Image();
+            var texture = new Texture2D(5, 5);
+            var pixels = new Color[25];
+            for (var i = 0; i < pixels.Length; i++)
+            {
+                pixels[i] = Color.red;
+            }
+            texture.SetPixels(pixels);
+            texture.Apply();
+            
+            _icon.style.backgroundImage = new StyleBackground(texture);
+            
+            Add(_icon);
+            Add(_label);
+        }
+        
+        public new class UxmlFactory : UxmlFactory<LabeledIcon, UxmlTraits> { }
     }
 }
