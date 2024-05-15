54b728307ba4a6bcd5453f81636e24bede65c6fc
Dan <danjm.dev@gmail.com>
2024-05-15 14:28:56 +1200

    added inline attributes to vector extension methods

--- Runtime/Extensions/Vector2Extensions.cs
+++ Runtime/Extensions/Vector2Extensions.cs
@@ -1,29 +1,37 @@
-﻿using Unity.Mathematics;
+﻿using System.Diagnostics.Contracts;
+using System.Runtime.CompilerServices;
+using Unity.Mathematics;
 using UnityEngine;
 
 namespace DJM.CoreTools.Extensions
 {
     public static class Vector2Extensions
     {
-        /// <summary>
-        /// Returns a Boolean indicating whether the current Vector2 is in a given range from another Vector2
-        /// </summary>
-        /// <param name="current">The current Vector2 position</param>
-        /// <param name="target">The Vector2 position to compare against</param>
-        /// <param name="range">The range value to compare against</param>
-        /// <returns>True if the current Vector2 is in the given range from the target Vector2, false otherwise</returns>
-        public static bool InRangeOf(this Vector2 current, Vector2 target, float range)
-        {
-            return (current - target).sqrMagnitude <= range * range;
-        }
-
+        [Pure, MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Vector3 X0Y(this Vector2 current, float y = 0f) => new(current.x, y, current.y);
+        
+        [Pure, MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Vector3 XY0(this Vector2 current, float z = 0f) => new(current.x, current.y, z);
         
+        
+        [Pure, MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static float3 X0YFloat(this Vector2 current, float y = 0f) => new(current.x, y, current.y);
+        
+        [Pure, MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static float3 XY0Float(this Vector2 current, float z = 0f) => new(current.x, current.y, z);
+        
+        
+        [Pure, MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Vector2 WithX(this Vector2 v, float x) => new(x, v.y);
+        
+        [Pure, MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Vector2 WithY(this Vector2 v, float y) => new(v.x, y);
         
+        
+        [Pure, MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Vector2 AddX(this Vector2 v, float x) => new(v.x + x, v.y);
+        
+        [Pure, MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Vector2 AddY(this Vector2 v, float y) => new(v.x, v.y + y);
     }
 }
--- Runtime/Extensions/Vector3Extensions.cs
+++ Runtime/Extensions/Vector3Extensions.cs
@@ -1,37 +1,56 @@
-﻿using UnityEngine;
+﻿using System.Diagnostics.Contracts;
+using System.Runtime.CompilerServices;
+using UnityEngine;
 
 namespace DJM.CoreTools.Extensions
 {
     public static class Vector3Extensions
     {
-        /// <summary>
-        /// Returns a Boolean indicating whether the current Vector3 is in a given range from another Vector3
-        /// </summary>
-        /// <param name="current">The current Vector3 position</param>
-        /// <param name="target">The Vector3 position to compare against</param>
-        /// <param name="range">The range value to compare against</param>
-        /// <returns>True if the current Vector3 is in the given range from the target Vector3, false otherwise</returns>
-        public static bool InRangeOf(this Vector3 current, Vector3 target, float range) 
-        {
-            return (current - target).sqrMagnitude <= range * range;
-        }
-        
+        [Pure, MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Vector2 XX(this Vector3 v) => new(v.x, v.x);
+        
+        [Pure, MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Vector2 XY(this Vector3 v) => new(v.x, v.y);
+        
+        [Pure, MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Vector2 XZ(this Vector3 v) => new(v.x, v.z);
+        
+        [Pure, MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Vector2 YX(this Vector3 v) => new(v.y, v.x);
+        
+        [Pure, MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Vector2 YY(this Vector3 v) => new(v.y, v.y);
+        
+        [Pure, MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Vector2 YZ(this Vector3 v) => new(v.y, v.z);
+        
+        [Pure, MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Vector2 ZX(this Vector3 v) => new(v.z, v.x);
+        
+        [Pure, MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Vector2 ZY(this Vector3 v) => new(v.z, v.y);
+        
+        [Pure, MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Vector2 ZZ(this Vector3 v) => new(v.z, v.z);
         
+        
+        [Pure, MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Vector3 WithX(this Vector3 v, float x) => new(x, v.y, v.z);
+        
+        [Pure, MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Vector3 WithY(this Vector3 v, float y) => new(v.x, y, v.z);
+        
+        [Pure, MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Vector3 WithZ(this Vector3 v, float z) => new(v.x, v.y, z);
         
+        
+        [Pure, MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Vector3 AddX(this Vector3 v, float x) => new(v.x + x, v.y, v.z);
+        
+        [Pure, MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Vector3 AddY(this Vector3 v, float y) => new(v.x, v.y + y, v.z);
+        
+        [Pure, MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static Vector3 AddZ(this Vector3 v, float z) => new(v.x, v.y, v.z + z);
     }
 }
