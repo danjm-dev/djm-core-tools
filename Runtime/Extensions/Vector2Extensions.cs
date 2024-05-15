e4b51bfac6710276a939787cac2fe119e14f5321
Dan <danjm.dev@gmail.com>
2024-05-15 14:10:59 +1200

    added methods to vector2/3 extensions

--- Runtime/Extensions/Vector2Extensions.cs
+++ Runtime/Extensions/Vector2Extensions.cs
@@ -17,14 +17,13 @@ namespace DJM.CoreTools.Extensions
             return (current - target).sqrMagnitude <= range * range;
         }
 
-        public static Vector3 X0Y(this Vector2 current, float y = 0f)
-        {
-            return new Vector3(current.x, y, current.y);
-        }
+        public static Vector3 X0Y(this Vector2 current, float y = 0f) => new(current.x, y, current.y);
+        public static Vector3 XY0(this Vector2 current, float z = 0f) => new(current.x, current.y, z);
         
-        public static Vector3 XY0(this Vector2 current, float z = 0f)
-        {
-            return new Vector3(current.x, current.y, z);
-        }
+        public static Vector2 WithX(this Vector2 v, float x) => new(x, v.y);
+        public static Vector2 WithY(this Vector2 v, float y) => new(v.x, y);
+        
+        public static Vector2 AddX(this Vector2 v, float x) => new(v.x + x, v.y);
+        public static Vector2 AddY(this Vector2 v, float y) => new(v.x, v.y + y);
     }
 }
--- Runtime/Extensions/Vector3Extensions.cs
+++ Runtime/Extensions/Vector3Extensions.cs
@@ -16,14 +16,22 @@ namespace DJM.CoreTools.Extensions
             return (current - target).sqrMagnitude <= range * range;
         }
         
-        public static Vector2 XY(this Vector3 current) 
-        {
-            return new Vector2(current.x, current.y);
-        }
+        public static Vector2 XX(this Vector3 v) => new(v.x, v.x);
+        public static Vector2 XY(this Vector3 v) => new(v.x, v.y);
+        public static Vector2 XZ(this Vector3 v) => new(v.x, v.z);
+        public static Vector2 YX(this Vector3 v) => new(v.y, v.x);
+        public static Vector2 YY(this Vector3 v) => new(v.y, v.y);
+        public static Vector2 YZ(this Vector3 v) => new(v.y, v.z);
+        public static Vector2 ZX(this Vector3 v) => new(v.z, v.x);
+        public static Vector2 ZY(this Vector3 v) => new(v.z, v.y);
+        public static Vector2 ZZ(this Vector3 v) => new(v.z, v.z);
         
-        public static Vector2 XZ(this Vector3 current) 
-        {
-            return new Vector2(current.x, current.z);
-        }
+        public static Vector3 WithX(this Vector3 v, float x) => new(x, v.y, v.z);
+        public static Vector3 WithY(this Vector3 v, float y) => new(v.x, y, v.z);
+        public static Vector3 WithZ(this Vector3 v, float z) => new(v.x, v.y, z);
+        
+        public static Vector3 AddX(this Vector3 v, float x) => new(v.x + x, v.y, v.z);
+        public static Vector3 AddY(this Vector3 v, float y) => new(v.x, v.y + y, v.z);
+        public static Vector3 AddZ(this Vector3 v, float z) => new(v.x, v.y, v.z + z);
     }
 }
